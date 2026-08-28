a64803370a44a1487cf7af8bd1ec9e47c8cab1f7
Nishant Yadav <113028102+nishanty23@users.noreply.github.com>
2026-08-28 17:26:13 +0530

    resolved the issues

--- assignment_one/bonus/structPadding.cpp
+++ assignment_one/bonus/structPadding.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 struct Layout1{
     char c1;
-    int i;
+    int i; // char is followed by an int, so extra padding will be added before the int to align it correctly
     char c2;
 };
 
@@ -14,18 +14,19 @@ struct Layout2{
 };
 
 int main(){
-    cout<<"Size of Layout1 : "<<sizeof(Layout1)<<endl;
-    cout<<"Size of Layout2 : "<<sizeof(Layout2)<<endl;
+    cout<<"Size of Layout1 : "<<sizeof(Layout1)<<endl; // (2+2) + 4 + (2+2) = 12
+    cout<<"Size of Layout2 : "<<sizeof(Layout2)<<endl; // 4 + 2 + 2 = 8
 
     /*
     Layout1 and Layout2 contain the same members, but their sizes can
-    be different because the compiler adds padding between members.
+    be different because the compiler adds padding between members
     Padding is extra memory added so that members are aligned properly
-    according to the requirements of the system.
-    In Layout1, the char is followed by an int, so extra padding may
-    be added before the int to align it correctly.
+    according to the requirements of the system
+    */
+
+    /*
     Member order matters when designing network packet headers or
-    hardware register maps because the exact memory layout can be important.
+    hardware register maps because the exact memory layout can be important
     */
 
     return 0;
