3563971874aade6c59a0319d3af2c404211bd8a1
Nishant Yadav <113028102+nishanty23@users.noreply.github.com>
2026-08-28 12:29:36 +0530

    removed the bonus solution

--- assignment_one/question_three/hrPayrollEngine.cpp
+++ assignment_one/question_three/hrPayrollEngine.cpp
@@ -224,33 +224,10 @@ public:
 
 int Employee::employeeCount = 0;
 
-struct Layout1{
-    char c1;
-    int i;
-    char c2;
-};
-
-struct Layout2{
-    int i;
-    char c1;
-    char c2;
-};
-
 int main(){
     Employee e1;
     Employee* e2 = new Employee();
     Employee* e3 = new Employee();
-
-    /*
-    e1.empId = 999;
-    e1.basicSalary = -1000;
-
-    These statements give a compiler error because empId and basicSalary
-    are private data members of the Employee class.
-    Private members can only be accessed through public member functions.
-    This provides encapsulation and prevents invalid data from being
-    directly changed from outside the class.
-    */
 
     e1.acceptDetails();
     e2->acceptDetails();
@@ -265,22 +242,7 @@ int main(){
     if(!e3->getIsActive()){
         cout<<e3->getName()<<" is no longer active. Payroll skipped."<<endl;
     }
-
     cout<<"Total Employees : "<<Employee::getEmployeeCount()<<endl;
-
-    cout<<"Size of Layout1 : "<<sizeof(Layout1)<<endl;
-    cout<<"Size of Layout2 : "<<sizeof(Layout2)<<endl;
-
-    /*
-    Layout1 and Layout2 contain the same members, but their sizes can
-    be different because the compiler adds padding between members.
-    Padding is extra memory added so that members are aligned properly
-    according to the requirements of the system.
-    In Layout1, the char is followed by an int, so extra padding may
-    be added before the int to align it correctly.
-    Member order matters when designing network packet headers or
-    hardware register maps because the exact memory layout can be important.
-    */
 
     delete e2;
     delete e3;
