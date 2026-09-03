473f8b64058ae48e0d8e83c03c64f028b30ce10f
Nishant Yadav <113028102+nishanty23@users.noreply.github.com>
2026-09-03 09:10:51 +0530

    Create typeAliases.cpp

---
+++ assignment_three/question_one/typeAliases.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+using namespace std;
+
+using Header = pair<string, string>;
+using HeaderList = vector<Header>;
+using Port = unsigned int;
+using IPAddress = string;
+
+typedef unsigned long long RequestId;
+
+void printHeaders(const HeaderList& headers){
+    cout<<"Headers:"<<endl;
+    int size = headers.size();
+    if(size == 0){
+        cout<<"HeaderList is empty"<<endl;
+        return;
+    }
+    for(int i=0; i<size; i++){
+        cout<<headers[i].first<<"    : "<<headers[i].second<<endl;
+    }
+}
+
+
+int main(){
+    HeaderList hList1;
+    Header h1("Content-Type","application/json");
+    Header h2("Authorization","Bearer eyJhbGci...");
+    Header h3("Accept-Language","en-US");
+    hList1.push_back(h1);
+    hList1.push_back(h2);
+    hList1.push_back(h3);
+    Port port;
+    RequestId reqId;
+    cout<<"Enter reqId: ";
+    cin>>reqId;
+    cout<<"Enter port: ";
+    cin>>port;
+    cout<<"Request ID    : "<<reqId<<endl;
+    cout<<"Server Port   : "<<port<<endl<<endl;
+    printHeaders(hList1);
+
+    return 0;
+}
